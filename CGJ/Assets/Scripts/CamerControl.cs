4a0ca1cea87209b235181a9b16a02828cc78124e
XTTD-2020RAURJA\Administrator <2643455336@qq.com>
2020-08-15 11:58:30 +0800

    角色控制

---
+++ CGJ/Assets/Scripts/CamerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerControl : MonoBehaviour
+{
+    public Transform target;
+    //边界
+    public float MinX;
+    public float MaxX;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 v = transform.position;
+        v.x = target.position.x;
+        //边界判断
+        if (v.x > MaxX)
+        {
+            v.x = MaxX;
+        }
+        else if (v.x < MinX)
+        {
+            v.x = MinX;
+        }
+        transform.position = v;
+    }
+}
