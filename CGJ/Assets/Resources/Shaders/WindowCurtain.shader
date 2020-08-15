c47650af0bddfe7df5be134188773bdc58e0b431
Xsoul1204 <nowqw@qq.com>
2020-08-16 04:04:01 +0800

    222

---
+++ CGJ/Assets/Resources/Shaders/WindowCurtain.shader
@@ -0,0 +1,53 @@
+﻿Shader "Custom/WindowCurtain"
+{
+    Properties
+    {
+        _MaskSize ("MaskSize", Range(0, 1)) = 1
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed _MaskSize;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed min = 0.5 - 0.5 * _MaskSize;
+                fixed max = 0.5 + 0.5 * _MaskSize;
+                if(i.uv.x > min && i.uv.x < max && i.uv.y > min && i.uv.y < max)
+                    discard;
+                return fixed4(0.0, 0.0 ,0.0 ,0.0);
+            }
+            ENDCG
+        }
+    }
+}
--- CGJ/Assets/Scripts/WindowCurtain.cs
+++ CGJ/Assets/Scripts/WindowCurtain.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
-public class WindowAlpha : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public class WindowCurtain : MonoBehaviour
 {
     #region Win函数常量
     private struct MARGINS
@@ -33,53 +35,75 @@ public class WindowAlpha : MonoBehaviour
     static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
     [DllImport("user32.dll")]
     private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-    private const int WS_POPUP = 0x800000;
     private const int GWL_EXSTYLE = -20;
     private const int GWL_STYLE = -16;
     private const int WS_EX_LAYERED = 0x00080000;
     private const int WS_BORDER = 0x00800000;
     private const int WS_CAPTION = 0x00C00000;
     private const int SWP_SHOWWINDOW = 0x0040;
-    private const int LWA_COLORKEY = 0x00000001;
-    private const int LWA_ALPHA = 0x00000002;
-    private const int WS_EX_TRANSPARENT = 0x20;
-    //
-    private const int ULW_COLORKEY = 0x00000001;
-    private const int ULW_ALPHA = 0x00000002;
-    private const int ULW_OPAQUE = 0x00000004;
-    private const int ULW_EX_NORESIZE = 0x00000008;
     #endregion
 
-    public int windowWidth;//窗口宽度
-    public int windowHeight;//窗口高度
-
-    private IntPtr hwnd;
-    private int posX;
-    private int posY;
-
-    void Awake()
+    private static WindowCurtain _Instance;
+    public static WindowCurtain Get()
     {
-#if UNITY_EDITOR
-        print("编辑模式无法使用！");
-#else
-        hwnd = GetActiveWindow();
-        posX = Screen.currentResolution.width / 2 - windowWidth / 2;
-        posY = Screen.currentResolution.height / 2 - windowHeight / 2;
-        SetWindowPos(hwnd, -1, posX, posY, windowWidth, windowHeight, SWP_SHOWWINDOW);
-
-        SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_BORDER & ~WS_CAPTION);SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_BORDER & ~WS_CAPTION);
-     
-        var margins = new MARGINS() { cxLeftWidth = -1 };
-        DwmExtendFrameIntoClientArea(hwnd, ref margins);
-#endif
+        return _Instance;
     }
 
-    void OnApplicationQuit()
+    private Image _image;
+
+    public float MaskSize
     {
-        //程序退出的时候设置窗体为0像素，从打开到走到awake也需要一定是时间
-        //会先有窗体边框，然后透明，这样会有闪一下的效果，
-        //设置窗体为0像素后，下次打开是就是0像素，走到awake再设置回来正常的窗口大小
-        //便能解决程序加载时会闪白色边框的现象
-        SetWindowPos(hwnd, -1, posX, posY, 0, 0, SWP_SHOWWINDOW);
+        get
+        {
+            var material = _image.material;
+            if (material != null)
+            {
+                return material.GetFloat("_MaskSize");
+            }
+            return 1.0F;
+        }
+
+        set
+        {
+            var material = _image.material;
+            if (material != null)
+            {
+                material.SetFloat("_MaskSize", value);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        _Instance = this;
+        _image = GetComponent<Image>();
+
+    }
+#if !UNITY_EDITOR
+    private void Awake()
+    {
+        var hwnd = GetActiveWindow();
+        //去边框并且透明
+        SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_BORDER & ~WS_CAPTION);
+        // 设置Margins
+        var margins = new MARGINS() { cxLeftWidth = -1 };
+        // 扩展Aero Glass
+        margins.cxLeftWidth = -1;
+        DwmExtendFrameIntoClientArea(hwnd, ref margins);
+}
+#endif
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (MaskSize < 1)
+                MaskSize += 0.01F;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (MaskSize > 0)
+                MaskSize -= 0.01F;
+        }
     }
 }
